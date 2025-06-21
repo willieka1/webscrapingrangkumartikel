590dd542aec3348ec92898b0a1e267b7f00ba4e7
willieka1 <williamekac12@gmail.com>
2025-06-21 17:31:47 +0700

    Update summarizer_app.py

--- summarizer_app.py
+++ summarizer_app.py
@@ -7,14 +7,11 @@ Original file is located at
     https://colab.research.google.com/drive/1xVLWKI2mteHMHaYk-kVgS3HljnEzM8nP
 """
 
-# Commented out IPython magic to ensure Python compatibility.
-# %pip install streamlit
-
 pip install requests beautifulsoup4 pandas python-dotenv groq
 
 pip install streamlit
 
-%pip install streamlit requests beautifulsoup4 groq
+pip install streamlit requests beautifulsoup4 groq
 
 import streamlit as st
 import streamlit as st
