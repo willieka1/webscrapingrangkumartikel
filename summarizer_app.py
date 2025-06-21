4c3bbaa94cd3a9c4c891d20840273636fc7dfe72
willieka1 <williamekac12@gmail.com>
2025-06-21 17:36:24 +0700

    summarizer_app.py

--- summarizer_app.py
+++ summarizer_app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1xVLWKI2mteHMHaYk-kVgS3HljnEzM8nP
 """
 
-%pip install requests beautifulsoup4 pandas python-dotenv groq
-
 %pip install streamlit
 
 %pip install streamlit requests beautifulsoup4 groq
